841694cd8b50b3340a0e58ace91230126ffe287b
upcwangying <upcwangying@126.com>
2020-07-12 17:21:49 +0800

    build($examples): examples

--- packages/hn-ui/examples/hn-ui-example/src/main.js
+++ packages/hn-ui/examples/hn-ui-example/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import HnUi from '@hn-ui/hn-ui'
 import '@hn-ui/theme/lib/index.css'
+import '@hn-ui/icons/lib/index.css'
 import App from './App.vue'
 
 Vue.config.productionTip = false
